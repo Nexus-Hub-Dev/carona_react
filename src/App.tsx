0728f0acc57c7b50395818d41e40fab4f72f91ab
João Victor Ribeiro <joao.vsantos2@gmail.com>
2026-09-04 11:39:41 -0300

    :sparkles: criação página caronas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar"
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from "react-toastify"
 import { AuthProvider } from "./contexts/AuthContext"
+import { Caronas } from "./pages/caronas/Caronas"
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 					<Navbar />
 					<div className="min-h-[80vh]">
 						<Routes>
+							<Route path="/caronas" element={<Caronas />} />
 						</Routes>
 					</div>
 					<Footer />
