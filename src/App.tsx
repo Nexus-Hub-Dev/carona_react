87e9f64e237ef6b4dfde8b080b14c7bd5f31e40f
Edson Silva <dinhovdp@gmail.com>
2026-09-08 10:16:30 -0300

    atualizado app.tsx resolvido conflitos

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
-import { AuthProvider } from "./contexts/AuthContext"
-import { AuthContext } from "./contexts/AuthContext"
+import { AuthContext, AuthProvider } from "./contexts/AuthContext"
 import Cadastro from "./pages/cadastro/Cadastro"
 import { Caronas } from "./pages/caronas/Caronas"
 import { CriarCarona } from "./pages/caronas/CriarCaronas"
@@ -13,6 +12,7 @@ import ContaPage from "./pages/conta/ContaPage"
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import Perfil from "./pages/perfil/Perfil"
+import Sobre from "./pages/sobre/Sobre"
 import Veiculos from "./pages/veiculos/Veiculos"
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
@@ -25,11 +25,6 @@ function ProtectedRoute({ children }: { children: React.ReactNode }) {
 
 	return children
 }
-import { Caronas } from "./pages/caronas/Caronas"
-import { CriarCarona } from "./pages/caronas/CriarCaronas"
-import Home from "./pages/home/Home"
-import Login from "./pages/login/Login"
-import Sobre from "./pages/sobre/Sobre"
 
 function AppContent() {
 	const { pathname } = useLocation()
@@ -40,9 +35,13 @@ function AppContent() {
 			{!isAuthenticationPage && <Navbar />}
 			<main className="flex flex-1 flex-col">
 				<Routes>
+					{/* Rotas Públicas */}
 					<Route path="/" element={<Navigate to="/login" replace />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/cadastro" element={<Cadastro />} />
+					<Route path="/sobre" element={<Sobre />} />
+
+					{/* Rotas Protegidas */}
 					<Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
 					<Route path="/caronas" element={<ProtectedRoute><Caronas /></ProtectedRoute>} />
 					<Route path="/oferecer-carona" element={<ProtectedRoute><CriarCarona /></ProtectedRoute>} />
@@ -50,26 +49,13 @@ function AppContent() {
 					<Route path="/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>} />
 					<Route path="/historico-caronas" element={<ProtectedRoute><ContaPage titulo="Histórico de caronas" descricao="Consulte suas viagens oferecidas e reservadas." /></ProtectedRoute>} />
 					<Route path="/dados-bancarios" element={<ProtectedRoute><ContaPage titulo="Dados bancários" descricao="Gerencie os dados usados para receber pelos seus trajetos." /></ProtectedRoute>} />
+					
+					{/* Fallback */}
 					<Route path="*" element={<ProtectedRoute><Navigate to="/home" replace /></ProtectedRoute>} />
 				</Routes>
 			</main>
 			{!isAuthenticationPage && <Footer />}
 		</div>
-		<>
-			{!isAuthenticationPage && <Navbar />}
-			<div className="min-h-[80vh]">
-				<Routes>
-					<Route path="/" element={<Navigate to="/home" replace />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/sobre" element={<Sobre />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/cadastro" element={<Cadastro />} />
-					<Route path="/caronas" element={<Caronas />} />
-					<Route path="/oferecer-carona" element={<CriarCarona />} />
-				</Routes>
-			</div>
-			{!isAuthenticationPage && <Footer />}
-		</>
 	)
 }
 
