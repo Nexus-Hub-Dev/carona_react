e474d7db3d033f9f9b2b73462184dc492a0152a1
paula_leao <paularegina396@gmail.com>
2026-09-08 11:06:28 -0300

    :sparkles: navbar, navicon e sobre atualizados

--- src/App.tsx
+++ src/App.tsx
@@ -12,50 +12,80 @@ import Veiculos from "./pages/veiculos/Veiculos"
 import Perfil from "./pages/perfil/Perfil"
 import ContaPage from "./pages/conta/ContaPage"
 
+// Certifique-se de criar este componente na pasta indicada:
+import Sobre from "./pages/sobre/Sobre" 
+
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
-	const { usuario } = useContext(AuthContext)
-	const location = useLocation()
-
-	if (!usuario.token) {
-		return <Navigate to="/login" replace state={{ from: location }} />
-	}
-
-	return children
+  const { usuario } = useContext(AuthContext)
+  const location = useLocation()
+  
+  if (!usuario?.token) {
+    return <Navigate to="/login" replace state={{ from: location }} />
+  }
+  return <>{children}</>
 }
 
 function AppContent() {
-	const { pathname } = useLocation()
-	const isAuthenticationPage = pathname === "/login" || pathname === "/cadastro"
-
-	return (
-		<div className="flex min-h-screen flex-col">
-				{!isAuthenticationPage && <Navbar />}
-				<main className="flex flex-1 flex-col">
-					<Routes>
-						<Route path="/" element={<Navigate to="/login" replace />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/cadastro" element={<Cadastro />} />
-						<Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-						<Route path="/caronas" element={<ProtectedRoute><Caronas /></ProtectedRoute>} />
-						<Route path="/oferecer-carona" element={<ProtectedRoute><CriarCarona /></ProtectedRoute>} />
-						<Route path="/veiculos" element={<ProtectedRoute><Veiculos /></ProtectedRoute>} />
-						<Route path="/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>} />
-						<Route path="/historico-caronas" element={<ProtectedRoute><ContaPage titulo="Histórico de caronas" descricao="Consulte suas viagens oferecidas e reservadas." /></ProtectedRoute>} />
-						<Route path="/dados-bancarios" element={<ProtectedRoute><ContaPage titulo="Dados bancários" descricao="Gerencie os dados usados para receber pelos seus trajetos." /></ProtectedRoute>} />
-						<Route path="*" element={<ProtectedRoute><Navigate to="/home" replace /></ProtectedRoute>} />
-					</Routes>
-				</main>
-				{!isAuthenticationPage && <Footer />}
-		</div>
-	)
+  const { pathname } = useLocation()
+  const isAuthenticationPage = pathname === "/login" || pathname === "/cadastro"
+  
+  return (
+    <div className="flex min-h-screen flex-col">
+      {!isAuthenticationPage && <Navbar />}
+      
+      <main className="flex flex-1 flex-col">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/caronas" element={<ProtectedRoute><Caronas /></ProtectedRoute>} />
+          <Route path="/oferecer-carona" element={<ProtectedRoute><CriarCarona /></ProtectedRoute>} />
+          <Route path="/veiculos" element={<ProtectedRoute><Veiculos /></ProtectedRoute>} />
+          <Route path="/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>} />
+          
+          {/* NOVA ROTA ADICIONADA */}
+          <Route path="/sobre" element={<ProtectedRoute><Sobre /></ProtectedRoute>} />
+          
+          <Route 
+            path="/historico-caronas" 
+            element={
+              <ProtectedRoute>
+                <ContaPage 
+                  titulo="Histórico de caronas" 
+                  descricao="Consulte suas viagens oferecidas e reservadas." 
+                />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/dados-bancarios" 
+            element={
+              <ProtectedRoute>
+                <ContaPage 
+                  titulo="Dados bancários" 
+                  descricao="Gerencie os dados usados para receber pelos seus trajetos." 
+                />
+              </ProtectedRoute>
+            } 
+          />
+          
+          <Route path="*" element={<ProtectedRoute><Navigate to="/home" replace /></ProtectedRoute>} />
+        </Routes>
+      </main>
+      
+      {!isAuthenticationPage && <Footer />}
+    </div>
+  )
 }
 
 function App() {
-	return (
-		<BrowserRouter>
-			<AppContent />
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  )
 }
 
 export default App
