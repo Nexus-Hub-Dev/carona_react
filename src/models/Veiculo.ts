a11720952ea7b3f2fd8e7c0d74389fc3a3a577e5
Grupo01turmaJava85 <grupo1.java85@gmail.com>
2026-09-08 11:59:52 -0300

    Update Veiculo.ts

--- src/models/Veiculo.ts
+++ src/models/Veiculo.ts
@@ -1,13 +1,10 @@
 export interface Veiculo {
-	id: number;
-	modelo: string;
-	placa: string;
-	cor: string;
-	foto: string;
-	capacidade: number;
-	acessivelPcd: boolean;
-	ativo: boolean;
-	foto: string;
-	capacidade: number;
-	acessivelPcd: boolean;
+    id: number;
+    modelo: string;
+    placa: string;
+    cor: string;
+    foto: string;
+    capacidade: number;
+    acessivelPcd: boolean;
+    ativo: boolean;
 }
